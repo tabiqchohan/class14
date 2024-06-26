27cd8f9e96afe13799ed1c06369a8672202c9313
tabiqchohan <160527853+tabiqchohan@users.noreply.github.com>
2024-06-26 10:17:20 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+/*function greeting(askName:()=>void){
+    console.log("hello greet")
+    askName()
+}
+function askName(){
+    console.log("Enter your name")
+}
+greeting(askName)*/
+//Asynchronous program (code)
+console.log("first");
+//bulit-in Asnchronous function
+setTimeout(function () {
+    console.log("second");
+}, 0);
+for (var i = 0; i <= 100000; i++) {
+    console.log("abc");
+}
+console.log("third");
